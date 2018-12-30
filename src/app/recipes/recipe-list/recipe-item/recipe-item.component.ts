e7b994cf7ecee2eabb27556843aff969c53c513f
sidra.sm18@gmail.com <sidra.sm18@gmail.com>
2018-12-30 23:12:46 +0530

    paragraph

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../../recipe.model';
 
 @Component({
@@ -8,10 +8,16 @@ import { Recipe } from '../../recipe.model';
 })
 export class RecipeItemComponent implements OnInit {
 
-  @Input() recipeElement:Recipe;
+ @Input() recipeElement:Recipe;
+ @Output() selectedRecipe = new EventEmitter<void>();
   constructor() { }
 
   ngOnInit() {
+  }  
+
+  onSelect()
+  {
+    this.selectedRecipe.emit();
   }
 
 }
