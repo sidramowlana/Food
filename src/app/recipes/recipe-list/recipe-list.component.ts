e33959a37f78c90aec16a024d0ce3764d800c520
sidra.sm18@gmail.com <sidra.sm18@gmail.com>
2018-12-30 23:05:26 +0530

    Property & Event Binding: Display recipe details when selecting recipe from list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,12 +7,13 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes:Recipe[]=[
-    new Recipe("Chocolate Fudge Cake","Chocolaty",
+    new Recipe("Chocolate Fudge Cake One","Chocolaty",
     "https://s3-eu-west-1.amazonaws.com/s3.mediafileserver.co.uk/carnation/WebFiles/RecipeImages/carnationchocolatefudgecake_lg.jpg")
-    ,new Recipe("Chocolate Fudge Cake","Chocolaty",
+    ,new Recipe("Chocolate Fudge Cake Two","Chocolaty",
     "https://s3-eu-west-1.amazonaws.com/s3.mediafileserver.co.uk/carnation/WebFiles/RecipeImages/carnationchocolatefudgecake_lg.jpg")
-    ,new Recipe("Chocolate Fudge Cake","Chocolaty",
+    ,new Recipe("Chocolate Fudge Cake Three","Chocolaty",
     "https://s3-eu-west-1.amazonaws.com/s3.mediafileserver.co.uk/carnation/WebFiles/RecipeImages/carnationchocolatefudgecake_lg.jpg")
     ];
   //stores recipe objects
@@ -20,5 +21,11 @@ export class RecipeListComponent implements OnInit {
 
   ngOnInit() {
   }
+
+  onSelectRecipe(recipe:Recipe)
+  {
+    this.recipeWasSelected.emit(recipe);
+  }
+  
 
 }
--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from './recipe.model';
 
 @Component({
   selector: 'app-recipes',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipesComponent implements OnInit {
 
+  selectingRecipe:Recipe;
   constructor() { }
 
   ngOnInit() {
   }
+
+  onDisplayrecipe(recipe:Recipe)
+  {
+    this.selectingRecipe=recipe;
+  }
+  
 
 }
