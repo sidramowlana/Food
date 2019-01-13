835325cccb873972a614dd954eee8aae3db34e45
sidra.sm18@gmail.com <sidra.sm18@gmail.com>
2019-01-13 13:39:04 +0530

    Comments for binding

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -5,8 +5,8 @@ import { Directive, HostListener, HostBinding } from '@angular/core';
 })
 export class DropdownDirective{
 
-    @HostBinding('class.open') isOpen=false;
-    @HostListener('click') toggelOpen()
+    @HostBinding('class.open') isOpen=false; //using css
+    @HostListener('click') toggelOpen() //using events
     {
         this.isOpen = !this.isOpen;
     }
